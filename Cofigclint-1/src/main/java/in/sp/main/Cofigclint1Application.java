84fdec13f05de506e2a6fa848191dbf116810183
RaviKumar560 <rk5118754@gmail.com>
2025-07-19 22:02:27 +0530

    Add files via upload

---
+++ Cofigclint-1/src/main/java/in/sp/main/Cofigclint1Application.java
@@ -0,0 +1,13 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cofigclint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cofigclint1Application.class, args);
+	}
+
+}
